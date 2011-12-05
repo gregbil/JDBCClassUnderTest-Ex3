cccd8f8674f6e558a9478630bfb3e4b8278553de
Grzechu <Grzechu@Fenix>
2011-12-05 18:45:13 +0100

    jakieś testy dla mocków

--- Testing1/test/JdbcTesting/JDBCTestCase.java
+++ Testing1/test/JdbcTesting/JDBCTestCase.java
@@ -4,28 +4,58 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class JDBCTestCase {
 
-	@Test
+	@Test(expected=SQLException.class)
+	public void testSQLExceptionWhenCreateStetmentInGenerateValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Connection conn = mock(Connection.class);
+		when(conn.createStatement()).thenThrow(new SQLException());
+		classUnderTest.generateValue(conn);
+		
+	}
+	
+/*	@Test
 	public void testGenerateValue() {
 		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
 		Connection conn = mock(Connection.class);
-		try {
-			classUnderTest.generateValue(conn);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		try{
+		when(conn.createStatement()).thenThrow(new SQLException());
+		classUnderTest.generateValue(conn);
 		}
-		
-	}
+		catch (SQLException e) {
+			fail("Exeption occured");
+		}
+		classUnderTest.
+	}*/
 
+	@Test(expected=SQLException.class)
+	public void testSQLExceptionWhenExecuteUpdateInInsertValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Statement stmt = mock(Statement.class);
+		when(stmt.executeUpdate(anyString())).thenThrow(new SQLException());
+		classUnderTest.insertValue(stmt, 0); 			
+	}
+	
+	@Test(expected=SQLException.class)
+	public void testSQLExceptionWhencloseInInsertValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Statement stmt = mock(Statement.class);
+		doThrow(new SQLException()).when(stmt).close();
+		classUnderTest.insertValue(stmt, 0); 			
+	}
+	
 	@Test
-	public void testInsertValue() {
-		fail("Not yet implemented");
+	public void testInsertValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Statement stmt = mock(Statement.class);
+		classUnderTest.insertValue(stmt, 0); 
+		verify(stmt).close();
+		
 	}
 
 }
