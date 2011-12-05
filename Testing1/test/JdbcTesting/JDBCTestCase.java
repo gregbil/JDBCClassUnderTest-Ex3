821244f2840402e7e3a25de11e352b2a5c3f0228
Grzechu <Grzechu@Fenix>
2011-12-05 22:03:05 +0100

    java 1.6 Pozostają testy dla executeUpdate oraz exeption dla 2 close

--- Testing1/test/JdbcTesting/JDBCTestCase.java
+++ Testing1/test/JdbcTesting/JDBCTestCase.java
@@ -1,12 +1,18 @@
 package JdbcTesting;
 
-import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
 import java.sql.Statement;
+
 import org.junit.Test;
-import static org.mockito.Mockito.*;
 
 public class JDBCTestCase {
 
@@ -19,19 +25,31 @@ public class JDBCTestCase {
 		
 	}
 	
-/*	@Test
-	public void testGenerateValue() {
+	@Test(expected=SQLException.class)
+	public void testSQLExceptionWhenCloseInGenerateValue() throws SQLException {
 		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
 		Connection conn = mock(Connection.class);
-		try{
-		when(conn.createStatement()).thenThrow(new SQLException());
+		Statement stmt = mock(Statement.class);
+		doThrow(new SQLException()).when(stmt).close();
+		when(conn.createStatement()).thenReturn(stmt);
 		classUnderTest.generateValue(conn);
-		}
-		catch (SQLException e) {
-			fail("Exeption occured");
-		}
-		classUnderTest.
-	}*/
+		
+	}
+	
+	
+	
+	@Test
+	public void testCloseInGenerateValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Connection conn = mock(Connection.class);
+		Statement stmt = mock(Statement.class);
+		when(conn.createStatement()).thenReturn(stmt);
+		//Poprawnie wykonana metoda zamknie stmt 2 razy
+		classUnderTest.generateValue(conn);
+		verify(stmt, times(2)).close();
+		
+	}
+	
 
 	@Test(expected=SQLException.class)
 	public void testSQLExceptionWhenExecuteUpdateInInsertValue() throws SQLException {
@@ -49,13 +67,35 @@ public class JDBCTestCase {
 		classUnderTest.insertValue(stmt, 0); 			
 	}
 	
+	@Test(expected=SQLTimeoutException.class)
+	public void testSQLTimeoutExceptionWhenExecuteUpdateInInsertValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Statement stmt = mock(Statement.class);
+		when(stmt.executeUpdate(anyString())).thenThrow(new SQLTimeoutException());
+		classUnderTest.insertValue(stmt, 0); 			
+	}
+	
+	
 	@Test
-	public void testInsertValue() throws SQLException {
+	public void testStatementCloseInInsertValue() throws SQLException {
 		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
 		Statement stmt = mock(Statement.class);
 		classUnderTest.insertValue(stmt, 0); 
 		verify(stmt).close();
 		
 	}
+	
+	
+	@Test
+	public void testValueIncrementInGenerateValue() throws SQLException {
+		JDBCClassUnderTest classUnderTest = new JDBCClassUnderTest();
+		Statement stmt = mock(Statement.class);
+		Connection conn = mock(Connection.class);
+		when(conn.createStatement()).thenReturn(stmt);
+		classUnderTest.insertValue(stmt, 0);  
+		
+		
+	}
+	
 
 }
